f06ba3500c656f2d6dd11b0ca92139975ae34fa0
Shashank <shashankj677@gmail.com>
2025-04-06 07:31:58 +0545

    added application entry point

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/shashankj99/ticket-booking-api/handlers"
+	"github.com/shashankj99/ticket-booking-api/repositories"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName:      "Ticket Booking API",
+		ServerHeader: "Ticket Booking API Header",
+	})
+
+	// Repositories
+	eventRepo := repositories.NewEventRepository(nil)
+
+	// Routing
+	router := app.Group("/api")
+
+	// Handlers
+	handlers.NewEventHandler(router.Group("/events"), eventRepo)
+
+	app.Listen(":3000")
+}
